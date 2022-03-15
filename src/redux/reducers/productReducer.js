6369ec943dd1cbe58c371150e718d2c8bb7dccf2
Nicolas <nicolas.cuve@ynov.com>
2022-03-15 22:18:07 +0100

    add reduc reducers

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,17 @@
+import { ActionsTypes } from "../constants/actionsTypes"
+
+const initialState = {
+    products:[{
+        id:1,
+        title:"Object",
+        category:"Category"
+    }]
+}
+export const productReducer = (state, {type,payload})=>{
+    switch (type) {
+        case ActionsTypes.SET_PRODUCTS:
+            return state
+        default:
+            return state;
+    }
+}
