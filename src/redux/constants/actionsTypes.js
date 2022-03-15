079a6fe6a59644cf6fc9eae5af2da39abcfbae3b
Nicolas <nicolas.cuve@ynov.com>
2022-03-15 22:08:13 +0100

    add redux actions types

--- src/redux/constants/actionsTypes.js
+++ src/redux/constants/actionsTypes.js
@@ -1,1 +1,5 @@
-export const 
+export const ActionsTypes = {
+    SET_PRODUCTS : "SET_PRODUCTS",
+    SELECTED_PRODUCTS : "SELECTED_PRODUCT",
+    REMOVE_SELECTED_PRODUCT : "REMOVE_SELECTED_PRODUCT"
+}
