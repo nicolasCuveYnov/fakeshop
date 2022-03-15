341186d29f26f659a6628f5c8bd0e680a9c428d0
Nicolas <nicolas.cuve@ynov.com>
2022-03-15 22:48:09 +0100

    add react components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
+import Header from './containers/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Header />
     </div>
   );
 }
