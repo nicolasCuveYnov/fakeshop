0680d83d3416a2b88c68c993e51e9cd9402b091e
Nicolas <nicolas.cuve@ynov.com>
2022-03-16 09:53:49 +0100

    add axios to fetch products list

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import {useSelector} from 'react-redux'
 import ProductComponent from './ProductComponent'
+import axios from 'axios'
+
 
 const ProductListing = () =>{
     const products = useSelector((state)=> state)
-    console.log(products)
+    const fetchProducts = async () =>{
+        const response = await axios
+            .get("https://fakestoreapi.com/products")
+            .catch((err)=>{
+                console.log(err)
+            })
+            
+    }
+    useEffect(()=>{
+        fetchProducts()
+    },[])
     return(
         <div className="ui grid container">
             <ProductComponent/>
